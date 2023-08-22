7cd79c311ef6004691b7b5a31323a964d70ca83a
unknown <Test@PNI6W11075.net.plm.eds.com>
2023-08-22 12:44:36 +0530

    updated code

--- main.tf
+++ main.tf
@@ -51,6 +51,13 @@ resource "aws_security_group" "my_security_group" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   tags = {
     Name = "my-security-group"
   }
