2c1dd8f0500f1cd4c135c4f9625de48c5d2530b7
unknown <Test@PNI6W11075.net.plm.eds.com>
2023-08-22 12:46:12 +0530

    updated code

--- main.tf
+++ main.tf
@@ -44,12 +44,12 @@ resource "aws_security_group" "my_security_group" {
   name_prefix = "my-security-group"
   description = "Security group for my Lambda function"
 
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  #ingress {
+  #  from_port   = 443
+  #  to_port     = 443
+  #  protocol    = "tcp"
+  #  cidr_blocks = ["0.0.0.0/0"]
+  #}
 
   egress {
     from_port   = 0
