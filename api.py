aa026ecf7d3d60b100900f0352e3a1175e8cf816
unknown <Test@PNI6W11075.net.plm.eds.com>
2023-08-22 16:07:03 +0530

    updated code

--- api.py
+++ api.py
@@ -1,5 +1,5 @@
 import json
-from botocore.vendored import requests
+import re
 
 def lambda_handler(event, context):
     url = "https://ij92qpvpma.execute-api.eu-west-1.amazonaws.com/candidate-email_serverless_lambda_stage/data"
